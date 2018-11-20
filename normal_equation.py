6963c3be050803eecddaf7d911cf0cd4c2e8bd53
lchancri <lchancri@student.42.fr>
2018-11-20 18:20:25 +0100

    removed the print

--- normal_equation.py
+++ normal_equation.py
@@ -10,15 +10,12 @@ Y = np.reshape(Y, (np.size(Y), 1))
 X = [np.insert(row, 0, 1) for row in data.drop(["price"], axis=1).values]
 X = np.reshape(X, (line, col))
 XT = np.transpose(X)
-print(X)
-print(XT)
 theta = [[0.0] * col]
 theta = np.reshape(theta, (col, 1))
 size = np.size(theta)
 
 def normal_equation(X, XT, Y):
     ret = XT.dot(X)
-    print(ret)
     ret = np.linalg.inv(ret)
     ret = ret.dot(XT)
     ret = ret.dot(Y)
